5c21544921caab08a4813e46a0291123de59a369
zhao-jun <443042468@qq.com>
2018-08-09 20:32:16 +0800

    feat: istanbul

--- src/math.js
+++ src/math.js
@@ -1,5 +1,17 @@
 module.exports = {
     add: (...args) => args.reduce((prev, curr) => prev + curr),
 
-    mul: (...args) => args.reduce((prev, curr) => prev * curr)
+    mul: (...args) => args.reduce((prev, curr) => prev * curr),
+    // 测试istanbul覆盖率
+    cover: (a, b) => {
+      if (a > b) {
+        return a - b
+      } else if (a == b) {
+        return 0
+      } else {
+        return min(a, b)
+      }
+    }
 }
+
+const min = (a, b) => b - a
--- test/mocha.js
+++ test/mocha.js
@@ -2,13 +2,14 @@
 // --reporter spec指定测试报告的格式，默认spec，可以指定tap、nyan等
 // 命令行不配置参数可以在mocha.opts配置
 const {should, expect, assert} = require('chai');
-const {add, mul} = require('../src/math');
+const {add, mul, cover} = require('../src/math');
 
 describe('math测试', () => {
   describe('加法测试', () => {
     it('should return 5 when 2 + 5', () => {
       expect(add(2, 5)).to.equal(7)
     })
+
     it('should return -1 when 2 - 3', () => {
       expect(add(2, -3)).to.equal(-1)
     })
@@ -18,8 +19,23 @@ describe('math测试', () => {
     it('should return 6 when 2 + 3', () => {
       expect(mul(2, 3)).to.equal(6)
     })
+
     it('should return 14 when 2 * 7', () => {
       expect(mul(2, 7)).to.equal(14)
+    })
+  })
+
+  describe('覆盖率测试', () => {
+    it('should return 3 when cover(2, 1)', () => {
+      expect(cover(2, 1)).to.equal(1)
+    })
+
+    it('should return 1 when cover(1, 2)', () => {
+      expect(cover(1, 2)).to.equal(1)
+    })
+
+    it('should return 0 when cover(1, 1)', () => {
+      expect(cover(1, 1)).to.equal(0)
     })
   })
 })
